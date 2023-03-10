b5ef212e37b5c81a690b6eaf078b1b064e27bd71
Josserand667 <josserand.dinunzio@gmail.com>
2023-03-10 10:54:30 +0100

    exo fonctions 4

---
+++ Exercices_Fonctions/Exo_Fonctions_4.js
@@ -0,0 +1,8 @@
+// fonction qui tri un tableau de chaines de caractères par ordre alphabétique
+
+const trialpha = (tab) => {
+    return tab.sort()   // La fonction .sort permet de tri par ordre alphabétique les éléments
+}
+
+let montab = ['bonjour','je','suis','ramoneur']
+console.log(trialpha(montab))
