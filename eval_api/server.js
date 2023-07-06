292c8b5ec02c59097495c62b774b9871ae0c11a2
Josserand667 <josserand.dinunzio@gmail.com>
2023-07-06 16:14:50 +0200

    version finale de mon travail acharné

--- eval_api/server.js
+++ eval_api/server.js
@@ -4,14 +4,19 @@ const PORT = process.env.PORT || '3001';
 const app = express();
 
 
-app.use('/', (req, res, next) => {
-    res.send("Bienvenu jeune âme; bienvenue dans le monde des douzes");
-    next();
+//app.use('/test', (req, res, next) => {
+//    console.log('2eme middlware');
+//    res.send("<h1> c'est cool ça fonctionne pas </h1>");
+//})
+
+app.use('/resocmieux', (req, res, next) => {
+    console.log('3eme middlware');
+    res.send('<h1> Rick Rolled </h1> <hr> <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0&autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
 })
 
-app.use('/test', (req, res, next) => {
-    console.log('2eme middlware');
-    res.send("c'est cool ça fonctionne");
+app.use('/', (req, res, next) => {
+    console.log('1er middleware')
+    res.send("<h1>Bienvenu jeune âme; bienvenue dans le monde des douzes</h1> <hr> <a href='http://localhost:3001/resocmieux'>ne pas appuyer</a>");
 })
 
 app.listen(PORT, () =>
